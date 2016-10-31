fbc8bee0a75e82786bb01b38a3a010c34833542b
Asel Acebedo Romalde <aselromaldemodulo@gmail.com>
2016-10-31 15:56:31 +0100

    Al fin he conseguido encapsular el texto largo y que se mueva horizontalmente. Ejercicio terminado

--- app/src/main/java/com/android/asel/ej10layoutsextra1/LayoutExtraActivity.java
+++ app/src/main/java/com/android/asel/ej10layoutsextra1/LayoutExtraActivity.java
@@ -2,12 +2,27 @@ package com.android.asel.ej10layoutsextra1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LayoutExtraActivity extends AppCompatActivity {
+
+    private TextView textViewTextoLargo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_extra);
+
+        textViewTextoLargo = (TextView) findViewById(R.id.textViewTextoLargo);
+
+        textViewTextoLargo.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        textViewTextoLargo.setSingleLine(true);
+        textViewTextoLargo.setMarqueeRepeatLimit(5);
+        textViewTextoLargo.setSelected(true);
+        
+
     }
 }
