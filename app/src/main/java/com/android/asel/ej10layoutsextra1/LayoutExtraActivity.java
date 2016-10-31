bc9bbfd38c8d37156ac974bd0ea5d7967f9a73f3
Asel Acebedo Romalde <aselromaldemodulo@gmail.com>
2016-10-31 15:24:54 +0100

    Initial commit Imagen de user editada. Layout perfilado, y solo a falta de unos retoques finales.

---
+++ app/src/main/java/com/android/asel/ej10layoutsextra1/LayoutExtraActivity.java
@@ -0,0 +1,13 @@
+package com.android.asel.ej10layoutsextra1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LayoutExtraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_extra);
+    }
+}
